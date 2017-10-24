61ba4eef99550d9d292d0da7f5e1d70326b3f92c
xiongxiong <ximengwuheng@163.com>
2017-10-24 13:01:17 +0800

    added log option

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import {REHYDRATE} from 'redux-persist/constants';
 
-export default function createMigration(manifest, versionKey) {
-    versionSelector = (state) => state[versionKey] && state[versionKey].version;
-    versionSetter = (state, versionInfo) => {
+export default function createMigration(manifest, versionKey, options) {
+    const {log: isLog = false} = options;
+    const log = isLog ? (s) => console.log(s) : (s) => null;
+    const versionSelector = (state) => state[versionKey] && state[versionKey].version;
+    const versionSetter = (state, versionInfo) => {
         return {
             ...state,
             [versionKey]: {...versionInfo}
@@ -20,21 +22,21 @@ export default function createMigration(manifest, versionKey) {
     };
 
     const migrate = (state, version) => {
-        console.log("=====>>>>> old version -- " + version);
-        console.log(state);
+        log("=====>>>>> old version -- " + version);
+        log(state);
         const versions = manifest.migrations.map((elem) => elem.version);
         const incomingIndex = versions.indexOf(version);
         manifest.migrations.filter((elem, index) => index > incomingIndex).forEach((elem) => {
-            console.log("---> state before: ");
-            console.log(state);
+            log("---> state before: ");
+            log(state);
             state = manifest.migrate(state, elem.version);
             state = versionSetter(state, elem);
-            console.log("---> state after: ");
-            console.log(state);
-            console.log("===>>> migration success: " + elem.version);
+            log("---> state after: ");
+            log(state);
+            log("===>>> migration success: " + elem.version);
         })
-        console.log("=====>>>>> new version -- " + versionSelector(state));
-        console.log(state);
+        log("=====>>>>> new version -- " + versionSelector(state));
+        log(state);
         return state;
     };
 
